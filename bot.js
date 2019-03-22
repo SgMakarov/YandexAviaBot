06d8cf2d6d3e887e31bf0251501fd51197b51a7f
SgMakarov <42974693+SgMakarov@users.noreply.github.com>
2019-03-23 01:22:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = process.env.TOKEN
+const TOKEN = process.env.TOKEN
 const {exec} = require('child_process');
-const bot = new TelegramBot(token, {
+const bot = new TelegramBot(TOKEN, {
     polling: true
 });
 const initlalMessageRegExp = new RegExp('/tickets ([\\w|а-я|А-Я]+)\\s+([\\w|а-я|А-я]+)', 'i');
